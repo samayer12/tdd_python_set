8e8e100280bff33fcea3b9de0aacc611388c7ea2
Sam <samayer12@gmail.com>
2020-03-03 18:37:59 -0600

    First failing test

---
+++ Src/set.py
@@ -0,0 +1,7 @@
+class Set:
+
+    def create(self):
+        return []
+
+    def create(self, size):
+        return [None] * 10
---
+++ Test/test_set.py
@@ -0,0 +1,16 @@
+import unittest
+from Src.set import Set
+
+
+class MyTestCase(unittest.TestCase):
+    def test_default_create_returns_empty_set(self):
+        self.assertEqual([], Set.create(self))
+
+    def test_create_returns_default_contents(self):
+        self.assertEqual(([None] * 10), Set.create(self, 10))
+
+
+    # def test_create_returns_correct_length(self):
+    #     self.assertEqual(10, Set.create(10))
+if __name__ == '__main__':
+    unittest.main()
